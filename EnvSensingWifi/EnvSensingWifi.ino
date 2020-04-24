d36ae52fdaf17229992307017bdedc1352f21d65
Manuel DESTOUESSE <manuel.destouesse@itrachealth.com>
2020-04-24 17:42:23 -0400

    update wifi http client

--- EnvSensingWifi/EnvSensingWifi.ino
+++ EnvSensingWifi/EnvSensingWifi.ino
@@ -1,57 +1,40 @@
-/*
-  WiFi Web Server
-
- A simple web server that shows the value of the analog input pins.
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 13 July 2010
- by dlf (Metodo2 srl)
- modified 31 May 2012
- by Tom Igoe
-
- */
-
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <Wire.h>
-#include <Adafruit_BMP280.h>
+#include <HttpClient.h>
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "";        // your network SSID (name)
-char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+// your network SSID (name)
+char ssid[] = "manu-laet";   
+// your network password (use for WPA, or use as key for WEP)
+char pass[] = "06071972"; 
+// your network key Index number (needed only for WEP)   
+int keyIndex = 0;            
 
 int status = WL_IDLE_STATUS;
 
-WiFiServer server(80);
-// BMP 280 sensor
-#define BMP_SCK  (13)
-#define BMP_MISO (12)
-#define BMP_MOSI (11)
-#define BMP_CS   (10)
-Adafruit_BMP280 bmp;
+// Web server informations
+IPAddress server(192,168,1,215);
+int port = 3000;
+WiFiClient client;
+HttpClient client1 = HttpClient(client, server, port);
+String response;
+int statusCode = 0;
+
+// last time you connected to the server, in milliseconds
+unsigned long lastConnectionTime = 0;  
+// delay between updates, in milliseconds             
+const unsigned long postingInterval = 600L * 1000L; 
+
 
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+      // wait for serial port to connect. Needed for native USB port only
+    ; 
   }
 
- if (!bmp.begin()) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-    while (1);
-  }
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
@@ -59,8 +42,9 @@ void setup() {
     while (true);
   }
 
+  // Check WiFi firmware
   String fv = WiFi.firmwareVersion();
-  if (fv < "1.0.0") {
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
     Serial.println("Please upgrade the firmware");
   }
 
@@ -74,80 +58,44 @@ void setup() {
     // wait 10 seconds for connection:
     delay(10000);
   }
-  server.begin();
   // you're connected now, so print out the status:
   printWifiStatus();
-
- 
 }
 
 
 void loop() {
-  // listen for incoming clients
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-//          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-//            int sensorReading = analogRead(analogChannel);
-//            client.print("analog input ");
-//            client.print(analogChannel);
-//            client.print(" is ");
-//            client.print(sensorReading);
-//            client.println("<br />");
-//          }
-          client.print("temperature ");
-          client.print(bmp.readTemperature());
-          client.println("<br />");
-          
-          client.print("pressure ");
-          client.print(bmp.readPressure());
-          client.println("<br />");
-          int battery = analogRead(ADC_BATTERY);
-          int batteryLevel = map(battery, 0, 1023, 0, 100);
-          client.print("battery ");
-          client.print(batteryLevel);
-          client.println("<br />");
-          
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-
-    // close the connection:
-    client.stop();
-    Serial.println("client disonnected");
+  // if 600 seconds have passed since your last connection,
+  // then connect again and send data:
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
   }
 }
 
+// this method makes a HTTP connection to the server:
+void httpRequest() {
+  // Post request with JSON content in a string
+  Serial.println("making POST request");
+  String contentType = "application/json";
+  String postData = "{\"sensor_uuid\":\"123-123-000-002\",\"sensor_value\":102300}";
+
+  client1.post("/api/v1/sensor_readings", contentType, postData);
+
+  // read the status code and body of the response
+  statusCode = client1.responseStatusCode();
+  response = client1.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+  }
+}
 
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
