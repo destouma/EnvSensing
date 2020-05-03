e5405e527e2e7e5c70534e57774caa8e530b8622
Manuel DESTOUESSE <manuel.destouesse@itrachealth.com>
2020-05-03 19:13:30 -0400

    remove battery

--- EnvSensingRev2/EnvSensingRev2.ino
+++ EnvSensingRev2/EnvSensingRev2.ino
@@ -27,38 +27,33 @@ unsigned long pressure=0;
 unsigned long counter=0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
+  while (!Serial) {
+  }
 
-
-// check bmp280 sensor
   if (!bmp.begin()) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     
+                  Adafruit_BMP280::SAMPLING_X2,  
+                  Adafruit_BMP280::SAMPLING_X16,  
+                  Adafruit_BMP280::FILTER_X16,     
+                  Adafruit_BMP280::STANDBY_MS_500); 
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;); 
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
   display.display();
-  delay(2000); // Pause for 2 seconds
+  delay(2000);
 
   // Clear the buffer
   display.clearDisplay();
   display.display();
-  //testfillrect();      // Draw many lines
-  //testdrawchar(); 
 }
 
 void loop() {
@@ -81,20 +76,6 @@ void loop() {
   display.setCursor(5, 10);
   pressure = bmp.readPressure()/100;
   display.println(pressure);
-  display.display();
-
-  delay(1000);
-  display.clearDisplay();  
-  display.display();
-
-  delay(1000);
-  display.clearDisplay();  
-  display.setTextSize(4); 
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(5, 10);
-  battery = analogRead(ADC_BATTERY);
-  batteryLevel = map(battery, 0, 1023, 0, 100);
-  display.println(batteryLevel);
   display.display();
 
   delay(1000);
